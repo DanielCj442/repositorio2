f11d3cf7f0d61cfb927d9e76011397b71818d1b7
DanielCj442 <130365678+DanielCj442@users.noreply.github.com>
2023-04-17 13:49:51 +0200

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,7 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Procesando");
+        System.out.println("Procesando2");
         Salida.escribir();
     }
 }
